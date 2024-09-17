b5904a0cc8d0b2de7125373c1f930d20bd6c9a3e
Leonardo Christino <63653681+LeoChristino@users.noreply.github.com>
2024-09-16 21:20:23 -0300

    Add files via upload

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,13 +1,14 @@
 plugins {
     alias(libs.plugins.android.application)
+    id("com.google.gms.google-services")
 }
 
 android {
-    namespace = "br.com.etecia.academiatcc"
+    namespace = "com.example.academiatcc"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "br.com.etecia.academiatcc"
+        applicationId = "com.example.academiatcc"
         minSdk = 24
         targetSdk = 34
         versionCode = 1
@@ -37,7 +38,15 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(libs.firebase.auth)
+    implementation(libs.play.services.gcm)
+    implementation(libs.firebase.firestore)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    implementation(platform("com.google.firebase:firebase-bom:33.3.0"))
+    implementation("com.google.firebase:firebase-analytics")
+    implementation("com.google.firebase:firebase-firestore")
+    implementation ("com.google.android.material:material:1.12.0")
+    implementation("androidx.compose.material3:material3:1.1.1")
 }
