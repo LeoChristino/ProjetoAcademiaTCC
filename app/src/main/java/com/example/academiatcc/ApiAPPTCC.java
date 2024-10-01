9315dfd8c2452d29440339d6320808ac356dac2f
augustooxz <lucass.aaugustoo@gmail.com>
2024-10-01 19:52:40 -0300

    Adicao RequestHandler

---
+++ app/src/main/java/com/example/academiatcc/ApiAPPTCC.java
@@ -0,0 +1,10 @@
+package com.example.academiatcc;
+
+public class ApiAPPTCC {
+   // private static final String ROOT_URL = //http://10.67.96.95/heroeapi/v1/Api.php?apicall="; Local API!!!!!
+
+
+    private static final String URL_
+
+
+}
