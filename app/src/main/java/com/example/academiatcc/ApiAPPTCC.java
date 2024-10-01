256bf3278ac0df01962fe5a3adf0844afeba8224
augustooxz <lucass.aaugustoo@gmail.com>
2024-10-01 20:02:12 -0300

    Adicao API quase pronta no APP

--- app/src/main/java/com/example/academiatcc/ApiAPPTCC.java
+++ app/src/main/java/com/example/academiatcc/ApiAPPTCC.java
@@ -3,8 +3,26 @@ package com.example.academiatcc;
 public class ApiAPPTCC {
    // private static final String ROOT_URL = //http://10.67.96.95/heroeapi/v1/Api.php?apicall="; Local API!!!!!
 
+//Create
+    private static final String URL_createPersonal
+    private static final String URL_createAluno
+    private static final String URL_createLista
+    private static final String URL_createTreino
+    private static final String URL_createExercicio
+    private static final String URL_getPersonal
 
-    private static final String URL_
+//Get
+    private static final String URL_getAluno
+    private static final String URL_getLista
+    private static final String URL_gettreino
 
+//Update
+    private static final String URL_updateTreino
+    private static final String URL_updateLista
+    private static final String URL_updateExercicio
 
+//Delete
+    private static final String URL_deleteTreino
+    private static final String URL_deleteExercicio
+    private static final String URL_Lista
 }
