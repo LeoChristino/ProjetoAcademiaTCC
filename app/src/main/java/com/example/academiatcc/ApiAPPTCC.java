d4a14f3bb6f35c2f9d49d2b4b442bc3ee797590c
augustooxz <lucass.aaugustoo@gmail.com>
2024-10-01 20:12:17 -0300

    Adicao API quase pronta no APP

--- app/src/main/java/com/example/academiatcc/ApiAPPTCC.java
+++ app/src/main/java/com/example/academiatcc/ApiAPPTCC.java
@@ -1,22 +1,22 @@
 package com.example.academiatcc;
 
 public class ApiAPPTCC {
-   // private static final String ROOT_URL = //http://10.67.96.95/heroeapi/v1/Api.php?apicall="; Local API!!!!!
+   private static final String ROOT_URL = "http://10.67.96.95/heroeapi/v1/Api.php?apicall="; //Local API!!!!!
 
 //Create
-    private static final String URL_createPersonal
-    private static final String URL_createAluno
-    private static final String URL_createLista
-    private static final String URL_createTreino
-    private static final String URL_createExercicio
+    private static final String URL_createPersonal = ROOT_URL + "createPersonal";
+    private static final String URL_createAluno = ROOT_URL + "createAluno";
+    private static final String URL_createLista = ROOT_URL + "createLista";
+    private static final String URL_createTreino = ROOT_URL + "createTreino";
+    private static final String URL_createExercicio = ROOT_URL + "createExercicio";
 
 
 //Get
-    private static final String URL_getPersonal
-    private static final String URL_getAluno
-    private static final String URL_getLista
-    private static final String URL_gettreino
-    private static final String URL_getExercicio
+    private static final String URL_getPersonal = ROOT_URL + "getPersonal";
+    private static final String URL_getAluno = ROOT_URL + "getAluno";
+    private static final String URL_getLista = ROOT_URL + "getLista";
+    private static final String URL_gettreino = ROOT_URL + "getTreino";
+    private static final String URL_getExercicio = ROOT_URL + "getExercicio";
 //Update
     private static final String URL_updateTreino
     private static final String URL_updateLista
