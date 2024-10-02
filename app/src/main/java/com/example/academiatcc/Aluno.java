af84aeaef72f1a1cb1b73fb2aa61fcf9b5aeaf94
augustooxz <lucass.aaugustoo@gmail.com>
2024-10-01 22:03:00 -0300

    Implementacao GET/SETTER/CONSTRUCTOR

--- app/src/main/java/com/example/academiatcc/Aluno.java
+++ app/src/main/java/com/example/academiatcc/Aluno.java
@@ -1,4 +1,90 @@
 package com.example.academiatcc;
 
+
+//create table tbAlunos(
+//codAlun int not null auto_increment,
+//nome varchar(50) not null,
+//sexo char(1) default "M" check(sexo in('F','M')),
+//dataNasc date,
+//cell char(10),
+//email varchar(50) not null,
+//ftAlun varchar(200),
+//codPer int not null,
+//primary key (codAlun),
+//foreign key(codPer)references tbPersonal(codPer));
+
 public class Aluno {
+    private int CodAluno;
+    private String NomeAluno;
+    private String Sexo;
+    private int ftPerfil;
+    private String Celular;
+    private String Email;
+    private String DataNasc;
+
+    public int getCodAluno() {
+        return CodAluno;
+    }
+
+    public void setCodAluno(int codAluno) {
+        CodAluno = codAluno;
+    }
+
+    public String getNomeAluno() {
+        return NomeAluno;
+    }
+
+    public void setNomeAluno(String nomeAluno) {
+        NomeAluno = nomeAluno;
+    }
+
+    public String getSexo() {
+        return Sexo;
+    }
+
+    public void setSexo(String sexo) {
+        Sexo = sexo;
+    }
+
+    public int getFtPerfil() {
+        return ftPerfil;
+    }
+
+    public void setFtPerfil(int ftPerfil) {
+        this.ftPerfil = ftPerfil;
+    }
+
+    public String getCelular() {
+        return Celular;
+    }
+
+    public void setCelular(String celular) {
+        Celular = celular;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+
+    public String getDataNasc() {
+        return DataNasc;
+    }
+
+    public void setDataNasc(String dataNasc) {
+        DataNasc = dataNasc;
+    }
+
+    public Aluno(int codAluno, String nomeAluno, String sexo, int ftPerfil, String celular, String email, String dataNasc) {
+        CodAluno = codAluno;
+        NomeAluno = nomeAluno;
+        Sexo = sexo;
+        this.ftPerfil = ftPerfil;
+        Celular = celular;
+        Email = email;
+        DataNasc = dataNasc;
+    }
 }
--- app/src/main/java/com/example/academiatcc/Exercicio.java
+++ app/src/main/java/com/example/academiatcc/Exercicio.java
@@ -1,4 +1,19 @@
 package com.example.academiatcc;
 
+
+
+//create table tbExercicios(
+//codExe int not null auto_increment,
+//nomeExe varchar(50) not null,
+//descricao varchar(100) not null,
+//video varchar(200),
+//ftExe varchar(200),
+//codCat int not null,
+//primary key(codExe),
+//foreign key(codCat)references tbCategoria(codCat));
+
 public class Exercicio {
+private int CodExe;
+private int NomeExe;
+
 }
