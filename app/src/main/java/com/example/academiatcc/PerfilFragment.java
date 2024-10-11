4747c430171888c136be064f575788746d0d9dfb
leochristino <leo.chrissantos28@gmail.com>
2024-10-10 21:19:11 -0300

    Mudanças tela perfil e cadastro

--- app/src/main/java/com/example/academiatcc/PerfilFragment.java
+++ app/src/main/java/com/example/academiatcc/PerfilFragment.java
@@ -7,6 +7,9 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.google.firebase.firestore.core.UserData;
 
 public class PerfilFragment extends Fragment {
 
@@ -16,4 +19,5 @@ public class PerfilFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_perfil, container, false);
     }
+
 }
