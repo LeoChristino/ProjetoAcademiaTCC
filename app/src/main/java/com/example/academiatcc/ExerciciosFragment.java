256243cea480896ceaa00d5f6a87d12afeb7d8a6
LeoChristino <Leo.chrissantos28@gmail.com>
2024-10-30 20:53:12 -0300

    Resolvido2

--- app/src/main/java/com/example/academiatcc/ExerciciosFragment.java
+++ app/src/main/java/com/example/academiatcc/ExerciciosFragment.java
@@ -3,6 +3,8 @@ package com.example.academiatcc;
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
@@ -14,10 +16,29 @@ import android.view.ViewGroup;
 
 public class ExerciciosFragment extends Fragment {
 
+    CardView segunda, quarta;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
         return inflater.inflate(R.layout.fragment_exercicios, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        segunda = view.findViewById(R.id.CardViewSegunda);
+        quarta = view.findViewById(R.id.CardViewQuartaFeira);
+
+        segunda.setOnClickListener(v -> {
+            FragmentManager fragmentManager = requireActivity().getSupportFragmentManager();
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.frameLayout2, new TesteSegundaFeiraFragment()); // Substitua pelo ID do container do fragment
+            fragmentTransaction.addToBackStack(null);
+            fragmentTransaction.commit();
+        });
+
+        // Configure o listener de clique para o CardView "quarta" da mesma forma
     }
 }
